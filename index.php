2ec432358f8764760e3b727fca6ccf6c0265f650
Thierry Delbart <thierry@thde.pro>
2015-12-09 13:52:17 -0200

    Enable landing localized versions

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php include('landingPeaceLabs.html'); 
+<?php 
+
+if (isset($_GET['l']) && ($l = $_GET['l']) && file_exist('landingPeaceLabs_'.$l.'.html'))
+{
+
+	include('landingPeaceLabs_'.$l.'.html');
+
+}
+else
+{
+
+        include('landingPeaceLabs.html');
+
+}
